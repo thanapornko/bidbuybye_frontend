a9be5d28dc07d74f237e49699440261e4dfabd18
thanapornko <thanapornkopatta@gmail.com>
2023-03-08 19:33:39 +0700

    beforemerge

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -299,54 +299,55 @@ export default function EditProfile({ open, setOpen, toggleDrawer }) {
       </Drawer>
     </>
   );
-  //   return (
-  //     <>
-  //       {/* <!-- drawer component --> */}
-  //       <div
-  //         id="drawer-right-example"
-  //         className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-1/3 dark:bg-gray-800"
-  //         tabIndex="-1"
-  //         aria-labelledby="drawer-right-label"
-  //       >
-  //         <h5
-  //           id="drawer-right-label"
-  //           className="inline-flex items-center mb-4 text-base text-gray-500 dark:text-gray-400"
-  //         >
-  //           Profile
-  //         </h5>
-  //         <button
-  //           // onClick={() => setOpen(!open)}
-  //           type="button"
-  //           data-drawer-hide="drawer-right-example"
-  //           aria-controls="drawer-right-example"
-  //           className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-xs p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-  //         >
-  //           <svg
-  //             aria-hidden="true"
-  //             className="w-5 h-5"
-  //             fill="currentColor"
-  //             viewBox="0 0 20 20"
-  //             xmlns="http://www.w3.org/2000/svg"
-  //           >
-  //             <path
-  //               fillRule="evenodd"
-  //               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-  //               clipRule="evenodd"
-  //             ></path>
-  //           </svg>
-  //           <span className="sr-only">Close menu</span>
-  //         </button>
-  //         {/* content */}
-  //         <div className="text-center">
-  //           <button className="flex-col space-y-2">
-  //             <img
-  //               src={
-  //                 file
-  //                   ? URL.createObjectURL(file)
-  //                   : authenticatedUser.profilePicture || profile
-  //               }
-  //               className="m-auto h-32 w-32 rounded-full border text-gray-600"
-  //             />
-  //             <input
-  //               onChange={(e) => {
 }
+//   return (
+//     <>
+//       {/* <!-- drawer component --> */}
+//       <div
+//         id="drawer-right-example"
+//         className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-1/3 dark:bg-gray-800"
+//         tabIndex="-1"
+//         aria-labelledby="drawer-right-label"
+//       >
+//         <h5
+//           id="drawer-right-label"
+//           className="inline-flex items-center mb-4 text-base text-gray-500 dark:text-gray-400"
+//         >
+//           Profile
+//         </h5>
+//         <button
+//           // onClick={() => setOpen(!open)}
+//           type="button"
+//           data-drawer-hide="drawer-right-example"
+//           aria-controls="drawer-right-example"
+//           className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-xs p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+//         >
+//           <svg
+//             aria-hidden="true"
+//             className="w-5 h-5"
+//             fill="currentColor"
+//             viewBox="0 0 20 20"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//             <path
+//               fillRule="evenodd"
+//               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+//               clipRule="evenodd"
+//             ></path>
+//           </svg>
+//           <span className="sr-only">Close menu</span>
+//         </button>
+//         {/* content */}
+//         <div className="text-center">
+//           <button className="flex-col space-y-2">
+//             <img
+//               src={
+//                 file
+//                   ? URL.createObjectURL(file)
+//                   : authenticatedUser.profilePicture || profile
+//               }
+//               className="m-auto h-32 w-32 rounded-full border text-gray-600"
+//             />
+//             <input
+//               onChange={(e) => {
+// }
