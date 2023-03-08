178e4598cbe61b721f181fb668be9320aba716e6
markeykung009 <markeykung009@gmail.com>
2023-03-08 18:02:24 +0700

    edit admin

--- src/apis/admin-api.js
+++ src/apis/admin-api.js
@@ -2,8 +2,8 @@ import axios from '../config/axios';
 
 export const getOrder = () => axios.get('/admin/adminOrder');
 
-export const updateConfirm = (input) =>
-  axios.patch('/admin/statusUpdateConfirmed', input);
+export const updateShipped = (input) =>
+  axios.patch('/admin/statusUpdateShipped', input);
 
 export const updateArrived = (input) =>
   axios.patch('/admin/statusUpdateArrived', input);
@@ -11,5 +11,5 @@ export const updateArrived = (input) =>
 export const updateVerified = (input) =>
   axios.patch('/admin/statusUpdateVerified', input);
 
-export const updateCompleted = (input) =>
-  axios.patch('/admin/statusUpdateCompleted', input);
+export const updateCancel = (input) =>
+  axios.patch('/admin/statusUpdateCancel', input);
